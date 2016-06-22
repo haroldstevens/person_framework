7c8d8e2b42838f90a9734531959635d791fab99c
Harold <harold.stevens@hotmail.co.uk>
2016-06-22 15:39:07 +0000

    Emails and Phone numbers test passed

--- person.rb
+++ person.rb
@@ -1,14 +1,27 @@
 class Person
 
-  attr_reader :dob, :first_name, :surname
+  attr_reader :dob, :first_name, :surname, :emails, :phone_no
+
 
 	def initialize(fname, sname, dob = nil)
     @first_name = fname.capitalize
     @surname = sname.capitalize
     @dob = Date.parse dob if dob
+
+    @emails =[]
+    @phone_no =[]
   end
 
   def fullname
     "#{@first_name} #{@surname}"
   end
+
+  def add_email(e_mail)
+    @emails << e_mail
+  end
+
+  def add_phone_no(phone_no)
+    @phone_no << phone_no
+  end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -16,6 +16,24 @@ describe "Person class" do
     expect(person.dob).to be_nil
   end
 
-  
+  it 'should store and return emails in an array' do
+    person = Person.new("joe", "bloggs", "1 Jan 1990")
+
+    expect(person.emails).to eq []
+    person.add_email "joe@foo.com"
+    person.add_email "joe.bloggs@work.com"
+
+    expect(person.emails).to eq ["joe@foo.com", "joe.bloggs@work.com"]
+  end
+
+  it 'should store and return phone numbers in an array' do
+    person = Person.new("joe", "bloggs", "1 Jan 1990")
+
+    expect(person.phone_no).to eq []
+    person.add_phone_no "07982635473"
+    person.add_phone_no "07853648282"
+
+    expect(person.phone_no).to eq ["07982635473", "07853648282"]
+  end
 
 end
